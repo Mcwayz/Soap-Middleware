bf307c90e9f7d7df90481cbd6c35f9b9353a7e68
Niza Tembo <mcwayzj@gmail.com>
2024-06-17 12:44:00 +0200

    serializer

--- middleware/serializers.py
+++ middleware/serializers.py
@@ -3,17 +3,17 @@
 from rest_framework import serializers
 
 class B2CRequestSerializer(serializers.Serializer):
-    CommandID = serializers.CharField(max_length=50)
+    CommandID = serializers.CharField(max_length=100)
     OriginatorConversationID = serializers.CharField(max_length=50)
-    ThirdPartyID = serializers.CharField(max_length=50)
+    ThirdPartyID = serializers.CharField(max_length=100)
     Password = serializers.CharField(max_length=100)
     ResultURL = serializers.URLField()
-    Timestamp = serializers.CharField(max_length=14)
+    Timestamp = serializers.CharField(max_length=50)
     Identifier = serializers.CharField(max_length=50)
     SecurityCredential = serializers.CharField(max_length=100)
     ShortCode = serializers.CharField(max_length=10)
-    ReceiverIdentifier = serializers.CharField(max_length=50)
+    ReceiverIdentifier = serializers.CharField(max_length=100)
     Amount = serializers.CharField(max_length=10)
-    Currency = serializers.CharField(max_length=3)
-    ReasonType = serializers.CharField(max_length=20)
+    Currency = serializers.CharField(max_length=5)
+    ReasonType = serializers.CharField(max_length=100)
     Comment = serializers.CharField(max_length=100)
